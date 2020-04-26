04fbfcba66b513412a0bef4ee1a6aff079270a93
Daiya, Lokesh <lokesh.daiya@fisglobal.com>
2020-04-26 19:03:10 +0530

    Initial commit

---
+++ examples/convert-to-rtl.js
@@ -0,0 +1,25 @@
+const glob = require('glob');
+const fs  = require('fs');
+const scssfmt = require('scssfmt');
+const flipSCSS = require('../index');
+
+glob( `${__dirname}/**/*.{css,scss}`, (error, files) => {
+    if(error) throw error;
+    for (const filename of files) {
+        const content = fs.readFileSync(filename).toString();
+        if (content !== null && content !== undefined && content.length > 0) {
+            rtlContent = flipSCSS(content);
+            rtlContent =
+            `
+            ${content}
+
+
+            [dir='rtl'] {
+                ${rtlContent}
+            }
+            `
+            fs.writeFileSync(filename, scssfmt(rtlContent));
+        }
+    }
+    
+})
